4eeade67d07d3d9ff56b85f2b44e8fa7b7c3dac1
Takuya Ueno <uenotakuya19@gmail.com>
2019-10-13 15:12:20 +0900

    commit

---
+++ 11/src/FullName.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label>First Name</label>
+      <input type="text" class="form-control" v-model="firstName" />
+    </div>
+    <div class="form-group">
+      <label>Last Name</label>
+      <input type="text" class="form-control" v-model="lastName" />
+    </div>
+    <p>{{fullNameed}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  data: () => {
+    return {
+      firstName: "",
+      lastName: "",
+      fullName: ""
+    };
+  },
+  methods: {
+    fullNamed() {
+      this.fullName = this.firstName + " " + this.lastName;
+      this.$emit("input", this.fullName);
+    }
+  }
+};
+</script>
